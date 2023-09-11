a888ef2e3c36912f3f601008f059ba99e0cd58bb
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-09-11 14:24:43 +0330

    Add project files.

---
+++ Resume.Presenation/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿#region Using
+
+using Microsoft.AspNetCore.Mvc;
+using Resume.Presenation.Models;
+
+namespace Resume.Presenation.Controllers;
+
+#endregion
+
+public class AdminController : Controller
+{
+
+    #region Admin Dashboard
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(UserDTO user)
+    {
+        //Add Email And Username To The Data Base
+
+        return View();
+    }
+
+    #endregion
+
+    #region List Of Users
+
+    [HttpPost]
+    public IActionResult ListOfUsers(string Email, string Username)
+    {
+        //Add Email And Username To The Data Base
+
+        return View();
+    }
+
+    #endregion
+}
---
+++ Resume.Presenation/Models/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Resume.Presenation.Models
+{
+    public class UserDTO
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
