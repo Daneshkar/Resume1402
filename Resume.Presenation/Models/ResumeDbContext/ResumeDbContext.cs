e38b0a7d5051e4b3ba5762d2c243a76ee1d1734b
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-09-11 21:04:44 +0330

    add firs tables

---
+++ Resume.Presenation/Models/ResumeDbContext/ResumeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resume.Presenation.Models.Entities.Education;
+using Resume.Presenation.Models.Entities.Experience;
+using Resume.Presenation.Models.Entities.MySkills;
+
+namespace Resume.Presenation.Models.ResumeDbContext;
+
+public class ResumeDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=DESKTOP-JCN2C1I\\MSSQLSERVER2019;Initial Catalog=ResumeDaneshkar;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<Experience> Experiences { get; set; }
+
+    public DbSet<MySkills> MySkills { get; set; }
+
+    public DbSet<Education> Educations { get; set; }
+}
