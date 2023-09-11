03c397aebf4d0137e9bfff0113b1c066d3fd952b
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-09-11 19:20:21 +0330

    Initial Education , Experience , MySkills Models

---
+++ Resume.Presenation/Models/Entities/MySkills/MySkills.cs
@@ -0,0 +1,10 @@
+﻿namespace Resume.Presenation.Models.Entities.MySkills;
+
+public class MySkills
+{
+    public int Id { get; set; }
+
+    public  string SkillTille { get; set; }
+
+    public int Percentage { get; set; }
+}
