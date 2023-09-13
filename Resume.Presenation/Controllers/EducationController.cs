c2e20bc0f84deb26fd50ef9fb09522bde80dc26a
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-09-13 19:47:47 +0330

    end of education class

--- Resume.Presenation/Controllers/EducationController.cs
+++ Resume.Presenation/Controllers/EducationController.cs
@@ -1,12 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#region Usings 
 
-namespace Resume.Presenation.Controllers
+using Microsoft.AspNetCore.Mvc;
+using Resume.Presenation.Models.Entities.Education;
+using Resume.Presenation.Models.ResumeDbContext;
+
+namespace Resume.Presenation.Controllers;
+
+#endregion
+
+public class EducationController : Controller
 {
-    public class EducationController : Controller
+    #region Ctor
+
+    //Dependency Injection
+    private ResumeDbContext _context;
+
+    public EducationController(ResumeDbContext context )
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
     }
+
+    #endregion
+
+    #region List Of Educations
+
+    [HttpGet]
+    public IActionResult ListOfEducations()
+    {
+        //List Of Educations
+        var listOfEducations = _context.Educations.ToList();
+
+        //Get a Education
+        Education education2 = _context.Educations.OrderByDescending(p=> p.Id).First();
+
+        return View();
+    }
+
+    #endregion
+
+    #region Create An Education
+
+    public IActionResult CreateAnEducation()
+    {
+        #region Create Record
+
+        List<Education> educationList = new List<Education>();
+
+        Education education = new Education()
+        {
+            Id = 1,
+            EducationTitle = "Master",
+            EducationDuration = "2012",
+            Description = "Degree"
+        };
+
+        educationList.Add(education);
+
+        Education educationDataBase = new Education()
+        {
+            EducationTitle = "Sepehr",
+            EducationDuration = "20023",
+            Description = "Sepehr"
+        };
+
+        _context.Educations.Add(education);
+        _context.SaveChanges();
+
+        #endregion
+
+        return View();
+    }
+
+    #endregion
 }
--- Resume.Presenation/Program.cs
+++ Resume.Presenation/Program.cs
@@ -1,3 +1,5 @@
+using Resume.Presenation.Models.ResumeDbContext;
+
 namespace Resume.Presenation
 {
     public class Program
@@ -8,6 +10,8 @@ namespace Resume.Presenation
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ResumeDbContext>();
 
             var app = builder.Build();
 
