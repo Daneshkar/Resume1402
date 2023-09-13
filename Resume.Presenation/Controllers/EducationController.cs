0c6ca8a7208a46532cb6f9bd84adea6e0cf6522e
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-09-13 18:45:43 +0330

    Add Education Controller

---
+++ Resume.Presenation/Controllers/EducationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Presenation.Controllers
+{
+    public class EducationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Resume.Presenation/Models/ResumeDbContext/ResumeDbContext.cs
+++ Resume.Presenation/Models/ResumeDbContext/ResumeDbContext.cs
@@ -1,21 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿#region Using
+
+using Microsoft.EntityFrameworkCore;
 using Resume.Presenation.Models.Entities.Education;
 using Resume.Presenation.Models.Entities.Experience;
 using Resume.Presenation.Models.Entities.MySkills;
 
 namespace Resume.Presenation.Models.ResumeDbContext;
 
+#endregion
+
 public class ResumeDbContext : DbContext
 {
+    #region Ctor
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Data Source=DESKTOP-JCN2C1I\\MSSQLSERVER2019;Initial Catalog=ResumeDaneshkar;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
         base.OnConfiguring(optionsBuilder);
     }
 
+    #endregion
+
+    #region Db Sets
+
     public DbSet<Experience> Experiences { get; set; }
 
     public DbSet<MySkills> MySkills { get; set; }
 
     public DbSet<Education> Educations { get; set; }
+
+    #endregion
 }
