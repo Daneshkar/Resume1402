105347b0b432ac9d7976374281e94f85024fdc15
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-09-25 20:49:49 +0330

    Initial IEducationService And EducationService

--- Resume.Presenation/Controllers/EducationController.cs
+++ Resume.Presenation/Controllers/EducationController.cs
@@ -13,12 +13,8 @@ public class EducationController : Controller
 {
     #region Ctor
 
-    //Dependency Injection
-    private ResumeDbContext _context;
-
-    public EducationController(ResumeDbContext context )
+    public EducationController( )
     {
-        _context = context;
     }
 
     #endregion
@@ -28,10 +24,6 @@ public class EducationController : Controller
     [HttpGet]
     public async Task<IActionResult> ListOfEducations()
     {
-        //Get a Education
-        List<Education> education = await _context.Educations
-                                                  .ToListAsync();
-
         return View();
     }
 
@@ -41,23 +33,6 @@ public class EducationController : Controller
 
     public async Task<IActionResult> CreateAnEducation()
     {
-        #region Fill Education Instance
-
-        Education education = new Education();
-
-        education.EducationDuration = "2022-2023";
-        education.EducationTitle = "Military";
-        education.Description = "That was ... ";
-
-        #endregion
-
-        #region Add Recorde To The Data Base
-
-        await _context.Educations.AddAsync(education);
-        await _context.SaveChangesAsync();
-
-        #endregion
-
         return RedirectToAction(nameof(ListOfEducations));
     }
 
@@ -67,20 +42,6 @@ public class EducationController : Controller
 
     public async Task<IActionResult> DeleteAnEducation(int educationId)
     {
-        #region Find Current Record 
-
-        Education? education = await _context.Educations
-                                             .FirstOrDefaultAsync(p=> p.Id == educationId); 
-
-        #endregion
-
-        #region Remove Record 
-
-        _context.Educations.Remove(education);
-        await _context.SaveChangesAsync();
-
-        #endregion
-
         return RedirectToAction(nameof(ListOfEducations));
     }
 
