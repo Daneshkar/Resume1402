f4163673eb23129a50ecd0cdd330d09aa83460c7
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-10-10 21:07:39 +0330

    Add IoC

--- Resume.Presenation/Program.cs
+++ Resume.Presenation/Program.cs
@@ -1,4 +1,6 @@
 
+using Resume.Domain.RepositoryInterface;
+using Resume.Infrastructure.Repository;
 using Resume.Presenation.Models.ResumeDbContext;
 
 namespace Resume.Presenation
@@ -11,6 +13,9 @@ namespace Resume.Presenation
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IEducationRepository , EducationRepository>();
+            builder.Services.AddScoped<IExperienceRepository , ExperienceRepository>();
+            builder.Services.AddScoped<IMySkillsRepsitory , MySkillsRepository>();
 
             builder.Services.AddDbContext<ResumeDbContext>();
 
