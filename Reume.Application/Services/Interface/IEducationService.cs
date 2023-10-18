8ddb7357758659f1006ce912cb563609b85e5e88
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-10-18 21:41:32 +0330

    Speed Run Item

--- Reume.Application/Services/Interface/IEducationService.cs
+++ Reume.Application/Services/Interface/IEducationService.cs
@@ -1,4 +1,6 @@
 ﻿using Resume.Domain.Models.Entities.Education;
+using Reume.Application.DTOs.AdminSide.Education;
+using System.Collections.Generic;
 
 namespace Reume.Application.Services.Interface;
 
@@ -6,4 +8,15 @@ public interface IEducationService
 {
     //GetListOFEducations
     List<Education> GetListOFEducations();
+
+    //Get List Of An Education For Show in Admin Panel 
+    List<ListOFAnEducationAdminSideDTO> GetListOfAnEducationForShowinAdminPanel();
+
+    Task AddEducationToDataBase(CreateAnEducationAdminSideDTO model);
+
+    Task<Education> GetAnEducationByIdAsync(int educationId);
+
+    Task EditAnEducation(Education education);
+
+    Task DeleteAnEducation(Education education);
 }
